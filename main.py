a43167a740201d7d88b94b8eb05b78530704f857
BaamPark <baam.park.95@gmail.com>
2022-10-16 16:59:57 -0400

    start finding homography

--- main.py
+++ main.py
@@ -54,19 +54,28 @@ for m in distance_min_list:
     print("des1 index: ", m[1])
     print("des2 index: ", m[2])
 
-mark_matching_points(img_1, img_2, kp1, kp2, distance_min_list, 10)
+kp1_coordinate = [p.pt for p in kp1]
+kp2_coordinate = [p.pt for p in kp2]
 
-"""
-my algorithm totall same as below
-bf = cv2.BFMatcher(cv2.NORM_L2) #Brute-Force
-matches = bf.match(des1, des2)
-matches = sorted(matches, key = lambda x:x.distance)
+kp1_list = []
+kp2_list = []
+for i in range(10):
+    kp1_list.append(kp1_coordinate[distance_min_list[i][1]])
+    kp2_list.append(kp2_coordinate[distance_min_list[i][2]])
 
-for m in matches:
-    print("distnace: ", m.distance)
-    print("des1: ", m.queryIdx)
-    print("des2: ", m.trainIdx)
-"""
+b = 0
+for p in kp1_list:
+    cv2.circle(img1, (int(p[0]), int(p[1])), radius=3, color=(0, 255, 0), thickness=-1)
+    b += 1
+    cv2.putText(img1, str(b), (int(p[0]), int(p[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+
+c = 0
+for p in kp2_list:
+    cv2.circle(img2, (int(p[0]), int(p[1])), radius=3, color=(0, 255, 0), thickness=-1)
+    c += 1
+    cv2.putText(img2, str(c), (int(p[0]), int(p[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+
+#find four match
 
 cv2.imshow("window", img_1)
 cv2.imshow("window2", img_2)
