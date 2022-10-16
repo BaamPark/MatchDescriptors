ac4c3254dbbe8393939d95abf50bfd4e490494d2
BaamPark <baam.park.95@gmail.com>
2022-10-16 04:30:37 -0400

    add putText so as to mark number of each step

--- main.py
+++ main.py
@@ -60,12 +60,17 @@ for i in range(10):
    kp1_list.append(kp1_coordinate[distance_min_list[i][1]])
    kp2_list.append(kp2_coordinate[distance_min_list[i][2]])
 
+b = 0
 for p in kp1_list:
     cv2.circle(img1, (int(p[0]), int(p[1])), radius=20, color=(0,255,0), thickness=2)
+    b+=1
+    cv2.putText(img1, str(b),(int(p[0]), int(p[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0),2, cv2.LINE_AA)
 
+c = 0
 for p in kp2_list:
     cv2.circle(img2, (int(p[0]), int(p[1])), radius=20, color=(0, 255, 0), thickness=2)
-
+    c+=1
+    cv2.putText(img2, str(c), (int(p[0]), int(p[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 """
 my algorithm totall same as below
 bf = cv2.BFMatcher(cv2.NORM_L2) #Brute-Force
