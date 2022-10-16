13a88ce3d884237dfa25f98230c69babb0e6513b
BaamPark <baam.park.95@gmail.com>
2022-10-16 01:46:15 -0400

    initial verstion

---
+++ MatchPoint.py
@@ -0,0 +1,5 @@
+class MatchPoint:
+    def __init__(self, distance, des1_idx, des2_idx):
+        self.distance = distance
+        self.des1_idx = des1_idx
+        self.des2_idx = des2_idx
---
+++ main.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from MatchPoint import MatchPoint
+
+#reading image
+img1 = cv2.imread('img1.jpg')
+gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+
+img2 = cv2.imread('img2.jpg')
+gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+#keypoints
+sift1 = cv2.SIFT_create()
+kp1, des1 = sift1.detectAndCompute(gray1,None) #kp: list of key points, des:numpy array of shape num_keypointsx128
+print(len(kp1))
+print(des1.shape)
+
+sift2 = cv2.SIFT_create()
+kp2, des2 = sift2.detectAndCompute(gray2, None)
+print(len(kp2))
+print(des2.shape)
+
+img_1 = cv2.drawKeypoints(gray1,kp1,img1)
+img_2 = cv2.drawKeypoints(gray2, kp2, img2)
+
+
+distance_min_list = []
+index_des2 = []
+for i in range(len(des1)):
+    distance_list = []
+    for j in range(len(des2)):
+        distance_list.append(cv2.norm(des1[i], des2[j]))
+
+    min_val = distance_list[0]
+    min_index = 0
+    for k in range(1, len(distance_list)):
+        if distance_list[k] < min_val:
+            min_val = distance_list[k]
+            min_index = k
+    matchpt = MatchPoint(min_val, i, min_index)
+    distance_min_list.append(matchpt)
+    # distance_min_list.append(min_val)    #distance_min_list.append(min(distance_list))
+    # index_des2.append(min_index)
+
+# distance_min_list.sort()
+print(distance_min_list[0].distance)
+distance_min_list = sorted(distance_min_list, key = lambda a:a.distance)
+
+for m in distance_min_list:
+    print(m.distance)
+# print(len(distance_min_list))
+# print("index 2 is ", len(index_des2))
+
+# print("distnace is ", dist_l2)
+
+bf = cv2.BFMatcher(cv2.NORM_L2) #Brute-Force
+matches = bf.match(des1, des2)
+
+matches = sorted(matches, key = lambda x:x.distance)
+print("len of match is ",len(matches))
+for m in matches:
+    print("distance is ", m.distance) #the smaller is the distance, the better is the match
+    print("des1 index is ", m.trainIdx) #des2
+    print("des2 index is ", m.queryIdx) #des1
+
+img3 = cv2.drawMatches(gray1, kp1, gray2, kp2, matches[:10], None)
+#
+# cv2.imshow("Maching result", img3)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows
